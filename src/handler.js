6667dc18dde859447ab22efc533372098a73977c
Vidunram A R <64352864+Goose-Of-War@users.noreply.github.com>
2022-11-15 14:37:11 +0530

    Handler without app.get for all req
    And some deletions... Anyhoo...

--- src/handler.js
+++ src/handler.js
@@ -1,1 +1,14 @@
 // Handler. Nothing here yet
+
+module.exports = function handler (app) {
+	function get (req, res) {
+		res.send("Hello");
+	}
+
+	function post () {
+		res.send("Bye");
+	}
+
+	app.get(/.*/, get);
+	app.post(/.*/, post);
+};
--- src/honk.js
+++ src/honk.js
@@ -1,3 +1,15 @@
 // Main JS file
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const socketio = require('socket.io');
 
-console.log("Honk");
+const appHandler = require('./handler');
+
+const honk = express();
+appHandler(honk);
+const honkServer = http.createServer(honk);
+const PORT = require('./serverinfo').port ?? 6005;
+const io = socketio(honkServer);
+
+honkServer.listen(PORT, () => console.log(`Honking in port ${PORT}`));
