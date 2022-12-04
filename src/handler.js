aa7359bf705327bb007a2d82b7c6ba25b47f952f
Goose <64352864+Goose-Of-War@users.noreply.github.com>
2022-12-04 16:15:40 +0530

    Changed the handler

--- src/handler.js
+++ src/handler.js
@@ -4,62 +4,78 @@ const path  = require('path');
 const DBH = require('../database/database_handler');
 
 module.exports = function handler (app) {
-	async function get (req, res) {
+	// plain redirect ;-;
+	app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../templates/index.html')));
+
+	// Auth
+
+	// Sign-in
+	app.get('/signin', (req, res) => {
+		if (req.cookies.user) res.redirect('/success');
+		res.sendFile(path.join(__dirname, '../templates/signin.html'));
+	});
+
+	app.post('/signin', (req, res) => {
+		const { username, password } = req.body;
+		DBH.getUser({
+			username,
+			password: Tools.encodePassword(password)
+		}).then(user => {
+			if (user) {
+				res.cookie('user', username);
+				res.cookie('name', user.displayName ?? 'Anonymous');
+				res.redirect("/success");
+			} else {
+				res.send('Invalid creds.');
+			}
+		}).catch(err => console.log(err));
+	});
+
+	// Sign-up
+	app.get('/signup', (req, res) => {
+		if (req.cookies.user) res.redirect('/success');
+		res.sendFile(path.join(__dirname, '../templates/signup.html'));
+	});
+
+	app.post('/signup', (req, res) => {
+		const user = { username, displayName, emailAddress, password } = req.body;
+		if (Tools.checkUserInfo(user)) {
+			DBH.addNewUser(user).then(output => {
+				res.cookie('user', username);
+				res.cookie('name', displayName ?? 'Anonymous');
+				res.redirect("/success");
+			}).catch(err => console.log(err));
+		} else res.send('Invalid values');
+	})
+
+	// Successful sign-in/sign-up
+	app.get('/success', (req, res) => {
+		if (!req.cookies.user) res.redirect('/signin');
+		res.sendFile(path.join(__dirname, '../templates/signedin.html'));
+	});
+
+	// Chats
+	// The page of the chats
+	app.get(/\/chat\/.*/, (req, res) => {
 		const args = req.path.split("/");
-		switch (args[1]) {
-			case '': {
-				res.sendFile(path.join(__dirname, '../templates/index.html'));
-				break;
+		if (!req.cookies.user) return res.redirect('/signin');
+		DBH.getChats().then(sockets => {
+			if (!sockets.find(socket => socket === args[2])) {
+				return res.send(JSON.stringify({code: 404, data: 'Not found'}));
 			}
-			case 'chat': {
-				if (!req.cookies.user) return res.redirect('/signin');
-				try {
-					const sockets = await DBH.getChats();
-					if (!sockets.find(socket => socket === args[2])) {
-						return res.redirect('./'+args.filter((e, i) => i-1).join('/'));
-					}
-					return res.sendFile(path.join(__dirname, '../templates/chat.html'));
-				}
-				catch (err) {
-					console.log(err);
-					res.send(";-;");
-				}
-				break;
-			}
-			case 'signup': {
-				if (req.cookies.user) res.redirect('/success');
-				res.sendFile(path.join(__dirname, '../templates/signup.html'));
-				break;
-			}
-			case 'signin': {
-				if (req.cookies.user) res.redirect('/success');
-				res.sendFile(path.join(__dirname, '../templates/signin.html'));
-				break;
-			}
-			case 'success': {
-				res.sendFile(path.join(__dirname, '../templates/signedin.html'));
-				break;
-			}
-			default: {
-				res.sendFile(req.path);
-			}
-		};
-	};
+			return res.sendFile(path.join(__dirname, '../templates/chat.html'));
+		}).catch(err => console.log(err));
+	})
+	// To fetch the messages of the specific chat
+	app.post('/fetch-messages', (req, res) => {
+		console.log(req.body.socket);
+		DBH.fetchMessages(req.body.chat).then(messages => res.send(JSON.stringify(messages))).catch(err => console.log(err));
+	})
 
 	async function post (req, res) {
 		const args = req.path.split("/");
 		switch (args[1]) {
 			case 'fetch-messages': {
-				console.log(req.body.socket);
-				try {
-					const messages = await DBH.fetchMessages(req.body.chat);
-					res.send(JSON.stringify(messages));
-					break;
-				}
-				catch (err) { 
-					console.log(err); 
-					return res.send("Uh...");
-				}
 				break;
 			}
 			case 'send-message': {
@@ -87,45 +103,9 @@ module.exports = function handler (app) {
 				}
 				break;
 			}
-			case 'signup': {
-				const user = { username, displayName, emailAddress, password } = req.body;
-				if (Tools.checkUserInfo(user)) {
-					try {
-						await DBH.addNewUser(user);
-						res.cookie('user', username);
-						res.cookie('name', displayName ?? 'Anonymous');
-						res.redirect("/success");
-					}
-					catch (err) {
-						console.log(err);
-					}
-				}
-				break;
-			}
-			case 'signin': {
-				const { username, password } = req.body;
-				try {
-					const user = await DBH.getUser({
-						username,
-						password: Tools.encodePassword(password)
-					});
-					if (user) {
-						res.cookie('user', username);
-						res.cookie('name', user.displayName ?? 'Anonymous');
-						res.redirect("/success");
-					}
-				}
-				catch (err) {
-					console.log(err);
-				}
-				break;
-			}
 			default: {
 				res.send(";-;");
 			}
 		};
 	};
-
-	app.get(/.*/, get);
-	app.post(/.*/, post);
 };
